3179a94d47fd0f68a580ace68bcd7ca6d9d899de
Vishal Rana <vr@labstack.com>
2018-03-08 13:39:28 -0800

    Updated express and koa
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- test/express.js
+++ test/express.js
@@ -8,8 +8,7 @@ app.use(cube(process.env.LABSTACK_KEY, {
     batchSize: 1
 }))
 app.get('/', (req, res) => {
-    console.log(res.statusCode)
     res.send('Hello World!')
 })
 
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
--- test/koa.js
+++ test/koa.js
@@ -2,16 +2,16 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const labstack = require('..')
 
-const koa = new Koa();
-const app = new Router();
+const koa = new Koa()
+const app = new Router()
 const {cube} = labstack.koa
 
-app.get('/:name', async (ctx) => {
-  ctx.body = `Hello, ${ctx.params.name}!\n`;
-});
+app.get('/', ctx => {
+  ctx.body = 'Hello, World!'
+})
 
 koa.use(cube(process.env.LABSTACK_KEY, {
     batchSize: 1
 }))
-koa.use(app.routes());
-koa.listen(3001);
+koa.use(app.routes())
+koa.listen(3002)
