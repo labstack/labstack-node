2bbdd6c4213d4f7e3cc5615efc60c45e475472e9
Vishal Rana <vr@labstack.com>
2018-05-26 23:41:55 -0700

    Added hub
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   Client: require('./lib/client').Client,
   APIError: require('./lib/client').APIError,
+  Hub: require('./lib/hub'),
   express: require('./lib/express'),
   koa: require('./lib/koa')
 }
